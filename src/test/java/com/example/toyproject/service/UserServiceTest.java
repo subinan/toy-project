1c9ccb4feb8bd837f253a8db5d45189e34b1bd29
안수빈 <060657@naver.com>
2024-04-26 16:51:24 +0900

    Test: 서비스 테스트 케이스 추가

---
+++ src/test/java/com/example/toyproject/service/UserServiceTest.java
@@ -0,0 +1,154 @@
+package com.example.toyproject.service;
+
+import com.example.toyproject.domain.User;
+import com.example.toyproject.dto.*;
+import com.example.toyproject.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.*;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("유저 생성 Test")
+    void 유저_생성() {
+        // given
+        SignUpRequestDTO signUpRequestDTO = new SignUpRequestDTO();
+        signUpRequestDTO.setUserId("user");
+        signUpRequestDTO.setPassword("1234");
+        signUpRequestDTO.setNickname("닉네임");
+        signUpRequestDTO.setName("이름");
+        signUpRequestDTO.setPhoneNumber("010-1234-1234");
+        signUpRequestDTO.setEmail("user@a.c");
+
+        User user = new User("user", "1234", "닉네임", "이름", "010-1234-1234", "user@a.c");
+
+        Long fakeId = 1L;
+        ReflectionTestUtils.setField(user, "id", fakeId);
+
+        // mocking
+        given(userRepository.save(any()))
+                .willReturn(user);
+        given(userRepository.findById(fakeId))
+                .willReturn(Optional.ofNullable(user));
+
+        // when
+        userService.createUser(signUpRequestDTO);
+
+        // then
+        User findUser = userRepository.findById(fakeId).get();
+        assertEquals(user.getUserId(), findUser.getUserId());
+        assertEquals(user.getPassword(), findUser.getPassword());
+        assertEquals(user.getNickname(), findUser.getNickname());
+        assertEquals(user.getName(), findUser.getName());
+        assertEquals(user.getEmail(), findUser.getEmail());
+        assertEquals(user.getPhoneNumber(), findUser.getPhoneNumber());
+    }
+
+    @Test
+    @DisplayName("유저 조회 Test")
+    void 유저_조회() {
+        // given
+        Pageable pageable = PageRequest.of(0, 10);
+
+        List<User> userList = new ArrayList<>();
+        userList.add(new User("user1", "password", "nickname", "name", "010-1234-1234", "user1@a.c"));
+        userList.add(new User("user2", "password", "nickname", "name", "010-1234-1234", "user2@a.c"));
+
+        Page<User> userPage = new PageImpl<>(userList, pageable, userList.size());
+        when(userRepository.findAll(pageable)).thenReturn(userPage);
+
+        // when
+        UserListResponseDTO userListResponseDTO = userService.getUsers(pageable);
+
+        // then
+        assertThat(userListResponseDTO.getUsers().size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("getPageable 메서드 테스트: createdAt으로 정렬, 오름차순")
+    void 페이징_가입순_오름차순() {
+        // given
+        int page = 1;
+        int pageSize = 10;
+        String sortKey = "createdAt";
+        String sortOrder = "asc";
+
+        // when
+        Pageable pageable = userService.getPageable(page, pageSize, sortKey, sortOrder);
+
+        // then
+        assertEquals(PageRequest.of(page - 1, pageSize, Sort.by(Sort.Direction.ASC, "createdAt")), pageable);
+    }
+
+    @Test
+    @DisplayName("getPageable 메서드 테스트: createdAt으로 정렬, 내림차순")
+    void 페이징_가입순_내림차순() {
+        // given
+        int page = 1;
+        int pageSize = 10;
+        String sortKey = "createdAt";
+        String sortOrder = "desc";
+
+        // when
+        Pageable pageable = userService.getPageable(page, pageSize, sortKey, sortOrder);
+
+        // then
+        assertEquals(PageRequest.of(page - 1, pageSize, Sort.by(Sort.Direction.DESC, "createdAt")), pageable);
+    }
+
+    @Test
+    @DisplayName("getPageable 메서드 테스트: userId로 정렬, 오름차순")
+    void 페이징_아이디순_오름차순() {
+        // given
+        int page = 1;
+        int pageSize = 10;
+        String sortKey = "userId";
+        String sortOrder = "asc";
+
+        // when
+        Pageable pageable = userService.getPageable(page, pageSize, sortKey, sortOrder);
+
+        // then
+        assertEquals(PageRequest.of(page - 1, pageSize, Sort.by(Sort.Direction.ASC, "userId")), pageable);
+    }
+
+    @Test
+    @DisplayName("getPageable 메서드 테스트: userId로 정렬, 내림차순")
+    void 페이징_아이디_내림차순() {
+        // given
+        int page = 1;
+        int pageSize = 10;
+        String sortKey = "userId";
+        String sortOrder = "desc";
+
+        // when
+        Pageable pageable = userService.getPageable(page, pageSize, sortKey, sortOrder);
+
+        // then
+        assertEquals(PageRequest.of(page - 1, pageSize, Sort.by(Sort.Direction.DESC, "userId")), pageable);
+    }
+
+}
