6b0dc3eb77e214a2f513a708f6eab7a7fa20a7f0
안수빈 <060657@naver.com>
2024-04-27 00:39:52 +0900

    Feat: swagger 추가 #5

--- src/main/java/com/example/toyproject/dto/UserUpdateRequestDTO.java
+++ src/main/java/com/example/toyproject/dto/UserUpdateRequestDTO.java
@@ -1,6 +1,7 @@
 package com.example.toyproject.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,28 +17,32 @@ import javax.validation.constraints.Size;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserUpdateRequestDTO {
-
+    @Parameter(description = "비밀번호: 알파벳, 숫자, 특수문자를 전부 포함하여 8자 이상")
     @NotBlank
     @Size(min = 8, message = "비밀번호는 8자 이상만 허용합니다.")
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]+$",
             message = "비밀번호는 최소한 하나의 알파벳(대소문자), 숫자, 특수문자를 포함해야 합니다.")
     private String password;
 
+    @Parameter(description = "닉네임: 알파벳, 숫자, 한글 20자 이하")
     @NotBlank
     @Size(max = 20, message = "닉네임은 20자 이하만 허용합니다.")
     @Pattern(regexp = "^[a-zA-Z0-9가-힣]*$", message = "닉네임은 알파벳, 숫자, 한글만 사용 가능합니다.")
     private String nickname;
 
+    @Parameter(description = "이름: 알파벳, 한글 10자 이하")
     @NotBlank
     @Size(max = 10, message = "이름은 10자 이하만 허용합니다.")
     @Pattern(regexp = "^[a-zA-Z가-힣]*$", message = "이름은 알파벳과 한글만 사용 가능합니다.")
     private String name;
 
+    @Parameter(description = "핸드폰 번호")
     @JsonProperty("phone_number")
     @NotBlank
     @Pattern(regexp = "^01([016-9])-(?:\\d{3}|\\d{4})-\\d{4}$", message = "핸드폰 번호 형식과 일치하지 않습니다.")
     private String phoneNumber;
 
+    @Parameter(description = "이메일")
     @NotBlank
     @Email(message = "이메일 형식과 일치하지 않습니다.")
     private String email;
